c1cba452dd4df3a679057e927a6973af7f3842ab
mariusherlea <mariusherlea@gmail.com>
2023-11-11 11:01:11 +0200

    Commit with project stop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "Please click a button";
+
+  function handleSelect(selectedButton) {
+    // switch (selectedButton) {
+    //   case "components":
+    //     console.log(selectedButton);
+    //     break;
+    //   case "jsx":
+    //     console.log(selectedButton);
+    //     break;
+    //   case "props":
+    //     console.log(selectedButton);
+    //     break;
+    //   case "state":
+    //     console.log(selectedButton);
+    //     break;
+    // }
+    console.log(tabContent);
+    tabContent = selectedButton;
+  }
+  console.log("APP Component render");
   return (
     <div>
       <Header />
@@ -15,6 +37,18 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
